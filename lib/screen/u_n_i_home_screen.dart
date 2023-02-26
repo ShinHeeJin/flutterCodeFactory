b7143a4e8878513fd9a61c8fcd0d099dcb87b085
ShinHeeJin <sisesu92@gmail.com>
2023-02-26 18:30:15 +0900

    feature :  MediaQuery

--- lib/screen/u_n_i_home_screen.dart
+++ lib/screen/u_n_i_home_screen.dart
@@ -5,8 +5,37 @@ class UnIHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Home Screen"),
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        top: true,
+        bottom: false,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _DDay(),
+            _CoupleImage(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DDay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("DDay Widget");
+  }
+}
+
+class _CoupleImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "asset/img/middle_image.png",
+      height: MediaQuery.of(context).size.height / 2,
     );
   }
 }
