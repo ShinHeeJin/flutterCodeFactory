09f3a789cc9755d539350c598531d2decaf24756
ShinHeeJin <sisesu92@gmail.com>
2023-02-26 18:18:54 +0900

    feature : UnIHomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:second/screen/image_carousel_home_screen.dart';
+import 'package:second/screen/u_n_i_home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: ImageCarouselHomeScreen());
+    return const MaterialApp(home: UnIHomeScreen());
   }
 }
---
+++ lib/screen/u_n_i_home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UnIHomeScreen extends StatelessWidget {
+  const UnIHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("Home Screen"),
+    );
+  }
+}
