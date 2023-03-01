111e7ddfb29109e673db73144182c50d3b9f1dd0
ShinHeeJin <sisesu92@gmail.com>
2023-03-02 00:50:48 +0900

    feat : TabBarView & BottomNavigationBar

--- lib/screen/random_dice_home_screen.dart
+++ lib/screen/random_dice_home_screen.dart
@@ -9,45 +9,84 @@ class RandomDiceHomeScreen extends StatefulWidget {
 
 class _RandomDiceHomeScreenState extends State<RandomDiceHomeScreen>
     with TickerProviderStateMixin {
-  TabController? controller;
+  TabController? tabController;
 
   @override
   void initState() {
     super.initState();
-    controller = TabController(length: 2, vsync: this);
+    tabController = TabController(length: 2, vsync: this);
+
+    tabController!.addListener(tabListner);
+  }
+
+  tabListner() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    tabController!.removeListener(tabListner);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: TabBarView(
-        controller: controller,
+        controller: tabController,
         children: renderChildren(),
       ),
       bottomNavigationBar: renderBottomNavigation(),
     );
   }
-}
 
-List<Widget> renderChildren() {
-  return [];
-}
+  List<Widget> renderChildren() {
+    return [
+      Container(
+        child: const Center(
+          child: Text(
+            'Tab 1',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      Container(
+        child: const Center(
+          child: Text(
+            'Tab 2',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    ];
+  }
 
-BottomNavigationBar renderBottomNavigation() {
-  return BottomNavigationBar(
-    items: const [
-      BottomNavigationBarItem(
-        icon: Icon(
-          Icons.edgesensor_high_outlined,
+  BottomNavigationBar renderBottomNavigation() {
+    return BottomNavigationBar(
+      currentIndex: tabController!.index,
+      onTap: (value) {
+        setState(() {
+          tabController!.animateTo(value);
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.edgesensor_high_outlined,
+          ),
+          label: '주사위',
         ),
-        label: '주사위',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(
-          Icons.settings,
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.settings,
+          ),
+          label: '설정',
         ),
-        label: '설정',
-      ),
-    ],
-  );
+      ],
+    );
+  }
 }
