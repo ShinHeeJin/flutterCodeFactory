1dd76ae003365d64e5f8291a331bcc52522a4d20
ShinHeeJin <sisesu92@gmail.com>
2023-03-02 00:40:15 +0900

    feature : TabBarView, BottomNavigationBar, TickerProviderStateMixin

--- lib/screen/random_dice_home_screen.dart
+++ lib/screen/random_dice_home_screen.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
-class RandomDiceHomeScreen extends StatelessWidget {
+class RandomDiceHomeScreen extends StatefulWidget {
   const RandomDiceHomeScreen({super.key});
 
   @override
+  State<RandomDiceHomeScreen> createState() => _RandomDiceHomeScreenState();
+}
+
+class _RandomDiceHomeScreenState extends State<RandomDiceHomeScreen>
+    with TickerProviderStateMixin {
+  TabController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("home screen"),
+    return Scaffold(
+      body: TabBarView(
+        controller: controller,
+        children: renderChildren(),
+      ),
+      bottomNavigationBar: renderBottomNavigation(),
     );
   }
 }
+
+List<Widget> renderChildren() {
+  return [];
+}
+
+BottomNavigationBar renderBottomNavigation() {
+  return BottomNavigationBar(items: const []);
+}
+
+# 256쪽 작업중
